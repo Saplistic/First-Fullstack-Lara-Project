b2595a7a4f45496bc2c7bf4d7bf495e30a65fa19
Mat. Devski Carlier <carliermateo@outlook.be>
2023-06-12 22:37:54 +0200

    migration 'is_admin' mistake fix

--- database/migrations/2023_06_08_181341_create_likes_table.php
+++ database/migrations/2023_06_08_181341_create_likes_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->integer('post_id');
             $table->integer('user_id');
-            $table->boolean('isadmin')->default('false');
             $table->timestamps();
         });
     }
