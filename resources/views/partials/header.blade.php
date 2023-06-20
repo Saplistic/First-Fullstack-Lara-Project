ae2aeed4ebdf924c7097bcd85310316527069b26
Mat. Devski Carlier <carliermateo@outlook.be>
2023-06-20 13:55:15 +0200

    name change

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,7 @@
             <!-- Left Side Of Navbar -->
             <ul class="nav nav-pills">
                 <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="{{ route('posts.index') }}" class="nav-link">Posts</a></li>
+                <li class="nav-item"><a href="{{ route('posts.index') }}" class="nav-link">News posts</a></li>
                 <li class="nav-item"><a href="{{ route('FAQs') }}" class="nav-link">FAQs</a></li>
                 <li class="nav-item"><a href="{{ route('about') }}" class="nav-link">About</a></li>
             </ul>
